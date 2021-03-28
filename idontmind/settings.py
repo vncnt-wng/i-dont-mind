32e3b5519d70834718688ddcb62e68326994c334
Wang, Vincent H <vincent.wang19@imperial.ac.uk>
2021-03-28 21:45:55 +0100

    Adds STATIC_ROOT to settings.py

--- idontmind/settings.py
+++ idontmind/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 ASGI_APPLICATION = 'idontmind.asgi.application'
 
