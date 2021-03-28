8c2c0dc302de3f06cdd7c69a9c1de8a21b29002d
Wang, Vincent H <vincent.wang19@imperial.ac.uk>
2021-03-28 03:52:07 +0100

    Static files work

--- idontmind/urls.py
+++ idontmind/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vote.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
